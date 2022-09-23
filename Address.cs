c4a15e4a00be5e372bcc55aead7cc2b5606d2045
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-23 15:00:52 +0200

    add-address-class

---
+++ Address.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Address
+{
+    public Address(string name, string surname, string street, string city, string province, string zip)
+    {
+        Name = name;
+        Surname = surname;
+        Street = street;
+        City = city;
+        Province = province;
+        Zip = zip;
+    }
+
+    public string Name { get; }
+    public string Surname { get; }
+    public string Street { get; }
+    public string City { get; }
+    public string Province { get; }
+    public string Zip { get; }
+}
+
+
+
+
+
+
+
